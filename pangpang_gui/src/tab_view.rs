1eb27ebe90f789a209540b6517cfaf6413594629
sun <sunxubo@hotmail.com>
2022-01-02 17:34:12 +0800

    added tab bar scrolling support

--- pangpang_gui/src/tab_view.rs
+++ pangpang_gui/src/tab_view.rs
@@ -1,15 +1,16 @@
+use std::ops::Add;
 
 use eframe::egui;
 
 use crate::terminal_view::TerminalView;
 
-
 type TabViewType = TerminalView;
 type TabViewData = Vec<(String, TabViewType)>;
-
+const ITEM_WIDTH: f32 = 150.0;
 pub struct TabView {
     items: TabViewData,
     selected: usize,
+    offset: f32,
 }
 
 impl TabView {
@@ -17,39 +18,74 @@ impl TabView {
         Self {
             items: TabViewData::new(),
             selected: 0,
+            offset: 0.0,
         }
     }
 
     pub fn insert(&mut self, title: String, view: TabViewType) {
         self.items.push((title, view));
         self.selected = self.items.len() - 1;
+        self.offset = f32::INFINITY;
     }
 
-    fn paint_tab_bar(&mut self, ui: &mut egui::Ui) {
+    fn paint_tab_bar(&mut self, ui: &mut egui::Ui) -> bool {
         ui.allocate_ui_with_layout(
-            egui::vec2(ui.available_width(), ui.fonts().row_height(egui::TextStyle::Heading)),
+            egui::vec2(
+                ui.available_width(),
+                ui.fonts().row_height(egui::TextStyle::Heading),
+            ),
             egui::Layout::left_to_right(),
             |ui| {
-                let mut remove: Option<usize> = None;
-                for (i, item) in self.items.iter().enumerate() {
-                    let mut close = false;
-                    let mut click = false;
-                    ui.add(TabItem::new(&item.0, i == self.selected, &mut close, &mut click));
-                    if close {
-                        remove = Some(i);
+                let delta = ui.input().scroll_delta;
+                if (delta.x != 0.0 || delta.y != 0.0) &&  ui.rect_contains_pointer(ui.max_rect()) {
+                    let distance = if delta.y.abs() > delta.x.abs() {
+                        delta.y
+                    } else {
+                        delta.x
+                    };
+                    if self.offset == f32::INFINITY {
+                        self.offset = ui.max_rect().width()
                     }
-                    if click {
-                        self.selected = i;
+                    self.offset = self.offset.add(distance).min(ui.max_rect().width()).max(0.0);
+                }
+
+                let scroll = egui::ScrollArea::neither();
+                let scroll = if self.offset == f32::INFINITY {
+                    scroll.stick_to_right()
+                } else {
+                    scroll.horizontal_scroll_offset(self.offset)
+                };
+                let mut changed = false;
+                scroll.show(ui, |ui| {
+                    let mut remove: Option<usize> = None;
+                    for (i, item) in self.items.iter().enumerate() {
+                        let mut close = false;
+                        let mut click = false;
+                        ui.add(TabItem::new(
+                            &item.0,
+                            i == self.selected,
+                            &mut close,
+                            &mut click,
+                        ));
+                        if close {
+                            remove = Some(i);
+                            changed = true;
+                        }
+                        if click {
+                            self.selected = i;
+                            changed = true;
+                        }
                     }
-                }
-                if let Some(i) = remove {
-                    self.items.remove(i);
-                    if self.selected > 0 {
-                        self.selected -= 1;
-                    } 
-                }
-            }
-        );
+                    if let Some(i) = remove {
+                        self.items.remove(i);
+                        if self.selected > 0 {
+                            self.selected -= 1;
+                        }
+                    }
+                });
+                changed
+            },
+        ).inner
     }
 }
 
@@ -59,15 +95,18 @@ impl egui::Widget for &mut TabView {
             ui.available_size(),
             egui::Layout::top_down(egui::Align::LEFT),
             |ui| {
-                self.paint_tab_bar(ui);
+                let changed = self.paint_tab_bar(ui);
                 if let Some((_, view)) = self.items.get_mut(self.selected) {
-                    ui.add(view);
+                    let response = ui.add(view);
+                    if changed {
+                        response.request_focus();
+                    }
                 }
-            }
-        ).response
+            },
+        )
+        .response
     }
 }
-
 
 struct TabItem<'a> {
     title: &'a String,
@@ -77,7 +116,12 @@ struct TabItem<'a> {
 }
 
 impl<'a> TabItem<'a> {
-    pub fn new(title: &'a String, selected: bool, close: &'a mut bool, click: &'a mut bool) -> Self {
+    pub fn new(
+        title: &'a String,
+        selected: bool,
+        close: &'a mut bool,
+        click: &'a mut bool,
+    ) -> Self {
         Self {
             title,
             selected,
@@ -89,40 +133,47 @@ impl<'a> TabItem<'a> {
 
 impl<'a> egui::Widget for TabItem<'a> {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let width = ui.available_width().min(150.0);
         ui.allocate_ui_with_layout(
-            egui::vec2(width, ui.available_height()),
+            egui::vec2(ITEM_WIDTH, ui.available_height()),
             egui::Layout::right_to_left(),
             |ui| {
                 if self.selected {
-                    ui.painter().rect_filled(ui.available_rect_before_wrap(), 5.0, egui::Color32::DARK_GREEN);
+                    ui.painter().rect_filled(
+                        ui.available_rect_before_wrap(),
+                        5.0,
+                        egui::Color32::DARK_GREEN,
+                    );
                 } else {
-                    ui.painter().rect_stroke(ui.available_rect_before_wrap(), 5.0, ui.style().visuals.window_stroke());
+                    ui.painter().rect_stroke(
+                        ui.available_rect_before_wrap(),
+                        5.0,
+                        ui.style().visuals.window_stroke(),
+                    );
                 }
-                let btn = egui::Button::new(egui::RichText::new(char::from_u32(0x1f5d9).unwrap()).text_style(egui::TextStyle::Small))
-                    .frame(false);
+                let btn = egui::Button::new(
+                    egui::RichText::new(char::from_u32(0x1f5d9).unwrap())
+                        .text_style(egui::TextStyle::Small),
+                )
+                .frame(false);
                 if ui.add(btn).clicked() {
                     *self.close = true;
                 }
-                ui.with_layout(
-                    egui::Layout::left_to_right(),
-                    |ui| {
-                        let rect = ui.available_rect_before_wrap();
-                        ui.painter().text(
-                            rect.left_center(),
-                            egui::Align2::LEFT_CENTER,
-                            self.title,
-                            egui::TextStyle::Button,
-                            ui.style().visuals.text_color()
-                        );
-                        let response = ui.allocate_rect(rect, egui::Sense::click());
-                        if response.clicked() {
-                            *self.click = true;
-                        }
+                ui.with_layout(egui::Layout::left_to_right(), |ui| {
+                    let rect = ui.available_rect_before_wrap();
+                    ui.painter().text(
+                        rect.left_center(),
+                        egui::Align2::LEFT_CENTER,
+                        self.title,
+                        egui::TextStyle::Button,
+                        ui.style().visuals.text_color(),
+                    );
+                    let response = ui.allocate_rect(rect, egui::Sense::click());
+                    if response.clicked() {
+                        *self.click = true;
                     }
-                );
-            }
-        ).response
+                });
+            },
+        )
+        .response
     }
 }
-
--- pangpang_gui/src/terminal_view/mod.rs
+++ pangpang_gui/src/terminal_view/mod.rs
@@ -1,8 +1,10 @@
 use std::sync::Arc;
 
 use eframe::{egui, epi};
-use pangpang::{alacritty_terminal::{ansi::C0, term}, pangpang_run_sync::Mutex};
-
+use pangpang::{
+    alacritty_terminal::{ansi::C0, term},
+    pangpang_run_sync::Mutex,
+};
 
 mod terminal_render;
 
@@ -36,9 +38,11 @@ pub struct TerminalView {
     mouse_primary_key_down: bool,
 }
 
-
 impl TerminalView {
-    pub fn new(sender: pangpang::terminal::msg::PpTerminalMessageSender, frame: epi::Frame) -> Self {
+    pub fn new(
+        sender: pangpang::terminal::msg::PpTerminalMessageSender,
+        frame: epi::Frame,
+    ) -> Self {
         Self {
             render_state: Arc::new(Mutex::new(terminal_render::TerminalRender::new(frame))),
             sender,
@@ -53,7 +57,12 @@ impl TerminalView {
         }
     }
 
-    fn input_state(&mut self, ui: &egui::Ui, state: terminal_render::TerminalRender, galley: Arc<egui::Galley>) {
+    fn input_state(
+        &mut self,
+        ui: &egui::Ui,
+        state: terminal_render::TerminalRender,
+        galley: Arc<egui::Galley>,
+    ) {
         let mut input_sequence: Vec<u8> = Vec::new();
         let mut modifiers_state = egui::Modifiers::default();
         let mut cursor_mode = b'[';
@@ -62,7 +71,11 @@ impl TerminalView {
         }
         for e in &ui.input().events {
             match e {
-                egui::Event::Key{key, pressed, modifiers} if *pressed => {
+                egui::Event::Key {
+                    key,
+                    pressed,
+                    modifiers,
+                } if *pressed => {
                     modifiers_state = *modifiers;
                     match *key {
                         egui::Key::ArrowUp => {
@@ -161,78 +174,147 @@ impl TerminalView {
                         }
                     }
                 }
-                egui::Event::PointerButton{pos, button: egui::PointerButton::Secondary, pressed: true, modifiers: _} => {
-                    let cursor = galley.cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2()).pcursor;
-                    self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Copy(cursor.paragraph.try_into().unwrap(), cursor.offset));
+                egui::Event::PointerButton {
+                    pos,
+                    button: egui::PointerButton::Secondary,
+                    pressed: true,
+                    modifiers: _,
+                } => {
+                    let cursor = galley
+                        .cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2())
+                        .pcursor;
+                    self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Copy(
+                        cursor.paragraph.try_into().unwrap(),
+                        cursor.offset,
+                    ));
                 }
-                egui::Event::PointerButton{pos, button: egui::PointerButton::Primary, pressed, modifiers: _} => {
+                egui::Event::PointerButton {
+                    pos,
+                    button: egui::PointerButton::Primary,
+                    pressed,
+                    modifiers: _,
+                } => {
                     self.mouse_primary_key_down = *pressed;
                     if self.mouse_primary_key_down {
-                        let cursor = galley.cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2()).pcursor;
+                        let cursor = galley
+                            .cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2())
+                            .pcursor;
                         let paragraph: i32 = cursor.paragraph.try_into().unwrap();
                         let scroll: i32 = state.display_offset().try_into().unwrap();
                         let line = paragraph - scroll;
-                        self.write_pty(pangpang::terminal::msg::PpTerminalMessage::SelectionStart(line, cursor.offset));
+                        self.write_pty(pangpang::terminal::msg::PpTerminalMessage::SelectionStart(
+                            line,
+                            cursor.offset,
+                        ));
                     }
                 }
                 egui::Event::PointerMoved(pos) => {
                     if self.mouse_primary_key_down {
-                        let cursor = galley.cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2()).pcursor;
+                        let cursor = galley
+                            .cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2())
+                            .pcursor;
                         let paragraph: i32 = cursor.paragraph.try_into().unwrap();
                         let scroll: i32 = state.display_offset().try_into().unwrap();
                         let line = paragraph - scroll;
-                        self.write_pty(pangpang::terminal::msg::PpTerminalMessage::SelectionUpdate(line, cursor.offset));
+                        self.write_pty(
+                            pangpang::terminal::msg::PpTerminalMessage::SelectionUpdate(
+                                line,
+                                cursor.offset,
+                            ),
+                        );
                     }
                 }
                 _ => {}
             };
         }
         if !input_sequence.is_empty() {
-            self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Input(input_sequence));
+            self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Input(
+                input_sequence,
+            ));
         }
         let scroll_delta = ui.input().scroll_delta.y;
         if scroll_delta != 0.0 {
-            if let Some(pos) = ui.input().pointer.hover_pos() {
-                if self.mouse_primary_key_down {
-                    let cursor = galley.cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2()).pcursor;
-                    let paragraph: i32 = cursor.paragraph.try_into().unwrap();
-                    let scroll: i32 = state.display_offset().try_into().unwrap();
-                    let line = paragraph - scroll;
-                    self.write_pty(pangpang::terminal::msg::PpTerminalMessage::SelectionUpdate(line, cursor.offset));
+            if ui.rect_contains_pointer(ui.max_rect()) {
+                if let Some(pos) = ui.input().pointer.hover_pos() {
+                    if self.mouse_primary_key_down {
+                        let cursor = galley
+                            .cursor_from_pos(pos.to_vec2() - ui.min_rect().min.to_vec2())
+                            .pcursor;
+                        let paragraph: i32 = cursor.paragraph.try_into().unwrap();
+                        let scroll: i32 = state.display_offset().try_into().unwrap();
+                        let line = paragraph - scroll;
+                        self.write_pty(
+                            pangpang::terminal::msg::PpTerminalMessage::SelectionUpdate(
+                                line,
+                                cursor.offset,
+                            ),
+                        );
+                    }
                 }
+
+                self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Scroll(
+                    (scroll_delta / ui.fonts().row_height(egui::TextStyle::Monospace)) as i32,
+                ));
             }
-            
-            self.write_pty(pangpang::terminal::msg::PpTerminalMessage::Scroll((scroll_delta / ui.fonts().row_height(egui::TextStyle::Monospace)) as i32));
         }
     }
 
+    fn add_contents(&mut self, ui: &mut egui::Ui, response: &egui::Response) {
+        let state = self.render_state.blocking_lock().clone();
+
+        let where_to_put_background = ui.painter().add(egui::Shape::Noop);
+
+        let terminal_pos = response.rect.left_top();
+        let galley = ui.fonts().layout_job(state.layout());
+        let cursor_pos = galley
+            .pos_from_pcursor(egui::epaint::text::cursor::PCursor {
+                paragraph: state.cursor_pos().1,
+                offset: state.cursor_pos().0,
+                prefer_next_row: false,
+            })
+            .translate(terminal_pos.to_vec2())
+            .left_top();
+        ui.output().text_cursor_pos = Some(cursor_pos);
+        ui.painter().galley(terminal_pos, galley.clone());
+        let visuals = ui.style().interact(&response);
+        let shape = if response.has_focus() {
+            self.input_state(ui, state, galley);
+            egui::epaint::RectShape {
+                rect: response.rect,
+                corner_radius: visuals.corner_radius,
+                fill: egui::Color32::TRANSPARENT,
+                stroke: ui.visuals().selection.stroke,
+            }
+        } else {
+            egui::epaint::RectShape {
+                rect: response.rect,
+                corner_radius: visuals.corner_radius,
+                fill: egui::Color32::TRANSPARENT,
+                stroke: visuals.bg_stroke,
+            }
+        };
+        ui.painter().set(where_to_put_background, shape);
+        if self.window_size != response.rect.size() {
+            self.window_size = response.rect.size();
+            let cell_width = ui.fonts().glyph_width(egui::TextStyle::Monospace, 'x');
+            let cell_height = ui.fonts().row_height(egui::TextStyle::Monospace);
+            self.write_pty(pangpang::terminal::msg::PpTerminalMessage::ReSize(
+                (self.window_size.x / cell_width) as usize,
+                (self.window_size.y / cell_height) as usize,
+            ));
+        }
+    }
 }
 
 impl egui::Widget for &mut TerminalView {
     fn ui(self, ui: &mut egui::Ui) -> egui::Response {
-        let inner = ui.allocate_ui(ui.available_size(), |ui| {
-            let state = self.render_state.blocking_lock().clone();
-
-            let terminal_pos = ui.min_rect().min;
-            let galley = ui.fonts().layout_job(state.layout());
-            let cursor_pos = galley.pos_from_pcursor(egui::epaint::text::cursor::PCursor { paragraph: state.cursor_pos().1, offset: state.cursor_pos().0, prefer_next_row: false })
-                .translate(terminal_pos.to_vec2()).left_top();
-            ui.output().text_cursor_pos = Some(cursor_pos);
-            ui.painter().galley(terminal_pos, galley.clone());
-            self.input_state(ui, state, galley);
-            if self.window_size != ui.available_size() {
-                self.window_size = ui.available_size();
-                let cell_width = ui.fonts().glyph_width(egui::TextStyle::Monospace, 'x');
-                let cell_height = ui.fonts().row_height(egui::TextStyle::Monospace);
-                self.write_pty(pangpang::terminal::msg::PpTerminalMessage::ReSize(
-                    (self.window_size.x / cell_width) as usize,
-                    (self.window_size.y / cell_height) as usize
-                ));
-            }
-        });
-        ui.memory().request_focus(inner.response.id);
-        inner.response
+        let (id, rect) = ui.allocate_space(ui.available_size());
+        let mut child_ui = ui.child_ui(rect, *ui.layout());
+        let response = child_ui.interact(rect, id, egui::Sense::click_and_drag());
+        self.add_contents(&mut child_ui, &response);
+        if response.clicked() && !response.lost_focus() {
+            response.request_focus();
+        }
+        response
     }
 }
-
-
